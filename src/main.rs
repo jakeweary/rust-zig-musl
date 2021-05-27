e7face5072a23e07781a7ea7d268906a0f977f0b
- <5144598+-@users.noreply.github.com>
2021-05-28 02:46:15 +0300

    update

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,9 @@ use rusqlite::{Connection, Result};
 fn main() -> Result<()> {
   let db = Connection::open_in_memory()?;
 
-  let version = db
+  let version: String = db
     .prepare("select sqlite_version()")?
-    .query_row([], |row| row.get::<_, String>(0))?;
+    .query_row([], |row| row.get(0))?;
 
   Ok(println!("SQLite version: {}", version))
 }
